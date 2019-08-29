951909945bfba6e114f181df6abf8fd91632e3a5
p81061473525@gmail.com <p81061473525@gmail.com>
2019-08-30 02:57:04 +0800

    add payment_method function

--- config/routes.rb
+++ config/routes.rb
@@ -23,5 +23,10 @@ Rails.application.routes.draw do
      end
   end
   resources :cart_items
-  resources :orders
+  resources :orders do
+    member do
+      post :pay_with_alipay
+      post :pay_with_wechat
+    end
+  end
 end
